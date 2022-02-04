cca07e46dbb2ae6ec088755db5973f5b846e845c
yehbr <klchmmdv@gmail.com>
2022-02-04 19:40:51 +0700

    Add lib, change styles

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import App from './App'
+import GlobalStyles from './common/styles/GlobalStyles'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <GlobalStyles />
   </React.StrictMode>,
   document.getElementById('root')
 )
